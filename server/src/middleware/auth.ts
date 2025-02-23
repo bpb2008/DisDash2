545e9647854feb70fbf1c5a245d06b70f0213448
bpb2008 <brittanythompson08@gmail.com>
2025-02-23 09:51:56 -0600

    added client secret into auth.ts

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import { expressjwt } from "express-jwt";
 
 export const checkJwt = expressjwt({
-  secret: "randomstring",
+  secret: `${process.env.AUTH0_CLIENT_SECRET}`,
   audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ["RS256"],
